5647a40d1edca45ba47d3d76181897e0dc030244
河村 泰佑 <kata.kawa813@gmail.com>
2021-05-23 10:40:42 +0900

    add

--- src/api/apiGoogleAppScripe.js
+++ src/api/apiGoogleAppScripe.js
@@ -1,6 +1,14 @@
 import axios from "axios";
 
-//共通処理
+// 共通処理
 const gasScript = axios.create({
   headers: { "content-type": "application/x-www-form-urlencoded" },
+});
+
+// interceptors = Gasの中にerrorが含まれていたらrejectする
+gasScript.interceptors.response.use((res) => {
+  if (res.data.error) {
+    return Promise.reject(res.data.error);
+  }
+  return Promise.resolve(res);
 });
